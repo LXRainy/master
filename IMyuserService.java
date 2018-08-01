0d6c62a6e044be116a210ac76c82239a8bcaac15
LXRainy <1219686410@qq.com>
2018-08-01 15:30:04 +0800

    初始化

---
+++ IMyuserService.java
@@ -0,0 +1,9 @@
+package hbi.core.test.service;
+
+import com.hand.hap.core.ProxySelf;
+import com.hand.hap.system.service.IBaseService;
+import hbi.core.test.dto.Myuser;
+
+public interface IMyuserService extends IBaseService<Myuser>, ProxySelf<IMyuserService>{
+
+}
